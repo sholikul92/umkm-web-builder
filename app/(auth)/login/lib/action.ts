9d5fa11812e9029a2862a9f213d118d151329bae
sholikul92 <sholikulardian@gmail.com>
2025-12-22 16:35:27 +0700

    fix: redirect to dashboard when user success login

--- app/(auth)/login/lib/action.ts
+++ app/(auth)/login/lib/action.ts
@@ -24,7 +24,7 @@ export async function handleLogin(data: SignInType) {
     await signIn("credentials", {
       email,
       password,
-      redirectTo: "/",
+      redirectTo: "/dashboard",
     });
 
     return {
