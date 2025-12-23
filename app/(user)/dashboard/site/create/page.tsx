df1b712eb8af263123c06dd15c6cdbe4c1c3125b
sholikul92 <sholikulardian@gmail.com>
2025-12-23 14:32:57 +0700

    chore: add style to background

--- app/(user)/dashboard/site/create/page.tsx
+++ app/(user)/dashboard/site/create/page.tsx
@@ -2,13 +2,15 @@ import FormCreateSite from "./_components/FormCreateSite";
 
 export default function CreateSite() {
   return (
-    <section className='max-w-4xl mx-auto px-4 py-10 space-y-10'>
-      <section>
-        <h1 className='text-3xl font-bold mb-2'>Buat Website Bisnis Anda</h1>
-        <p className='text-gray-600'>Lengkapi informasi di bawah ini. Website dapat diedit kembali setelah dipublikasikan.</p>
-      </section>
+    <section className='bg-gray-50'>
+      <section className='max-w-4xl mx-auto px-4 py-10 space-y-10'>
+        <section>
+          <h1 className='text-3xl font-bold mb-2'>Buat Website Bisnis Anda</h1>
+          <p className='text-gray-600'>Lengkapi informasi di bawah ini. Website dapat diedit kembali setelah dipublikasikan.</p>
+        </section>
 
-      <FormCreateSite />
+        <FormCreateSite />
+      </section>
     </section>
   );
 }
