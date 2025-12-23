4aecb2240ed472b5a230ac1a2cbd5db4ec251c51
sholikul92 <sholikulardian@gmail.com>
2025-12-23 14:32:27 +0700

    chore: remove html tag

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,11 +4,9 @@ import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='id'>
-      <body>
-        <main>{children}</main>
-        <Toaster />
-      </body>
-    </html>
+    <>
+      <main>{children}</main>
+      <Toaster />
+    </>
   );
 }
