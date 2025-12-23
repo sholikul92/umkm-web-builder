d527aa1ff645c87c6c62c9e703a035c53525db84
sholikul92 <sholikulardian@gmail.com>
2025-12-23 14:32:40 +0700

    chore: remove header component from layout

--- app/(user)/dashboard/layout.tsx
+++ app/(user)/dashboard/layout.tsx
@@ -1,11 +1,5 @@
 import "@/app/globals.css";
-import SiteHeader from "./_components/SiteHeader";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <SiteHeader />
-      <main className='bg-gray-50 px-4 py-10 space-y-8'>{children}</main>
-    </>
-  );
+  return <main>{children}</main>;
 }
