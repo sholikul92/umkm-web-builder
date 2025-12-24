05ac551432428bc0981a07a885238e41aa4ab237
sholikul92 <sholikulardian@gmail.com>
2025-12-24 11:51:46 +0700

    chore: add function upload image to db

---
+++ lib/supabase/uploadProductImage.ts
@@ -0,0 +1,20 @@
+import { supabaseServer } from "./server";
+
+export async function uploadProductImage(file: File) {
+  const ext = file.name.split(".").pop();
+  const fileName = `${crypto.randomUUID()}.${ext}`;
+
+  const { error } = await supabaseServer.storage.from("product_images").upload(fileName, file, {
+    cacheControl: "3600",
+    upsert: false,
+  });
+
+  if (error) {
+    console.log(error.message);
+    throw new Error("Gagal upload gambar produk");
+  }
+
+  const { data } = supabaseServer.storage.from("product-images").getPublicUrl(fileName);
+
+  return data.publicUrl;
+}
